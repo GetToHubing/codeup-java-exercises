ca42e42ba2d5105610cd5fe51a6c3aa091124d79
David <davidbanker22@yahoo.com>
2023-03-24 09:59:52 -0500

    initial commit

---
+++ src/HelloWorld.java
@@ -0,0 +1,10 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.print("Hello, ");
+        System.out.print("World!");
+        int myFavoriteNumber = 777;
+        System.out.println(myFavoriteNumber);
+        String myString = "howdly doodly";
+        System.out.println(myString);
+    }
+}
