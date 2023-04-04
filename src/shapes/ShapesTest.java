c46c3fc4fc737fd5a2cdac528e51336433632dd7
David <davidbanker22@yahoo.com>
2023-04-04 15:24:56 -0500

    interfaces and abstract classes exercise

--- src/shapes/ShapesTest.java
+++ src/shapes/ShapesTest.java
@@ -1,13 +1,24 @@
 package shapes;
 
 public class ShapesTest {
-    public static void main(String[] args) {
-        Rectangle box1 = new Rectangle(4, 5);
-        System.out.println(box1.getArea());
-        System.out.println(box1.getPerimeter());
 
-        Rectangle box2 = new Square(5);
-        System.out.println(box2.getArea());
-        System.out.println(box2.getPerimeter());
+    static Measurable myShape = new Square(5);
+    static Measurable myRectangle = new Rectangle(5, 6);
+    public static void main(String[] args) {
+//        Rectangle box1 = new Rectangle(4, 5);
+//        System.out.println(box1.getArea());
+//        System.out.println(box1.getPerimeter());
+//
+//        Rectangle box2 = new Square(5);
+//        System.out.println(box2.getArea());
+//        System.out.println(box2.getPerimeter());
+
+        Quadrilateral box1 = new Square(5);
+        System.out.println(myShape.getArea());
+        System.out.println(myShape.getPerimeter());
+        System.out.println(myRectangle.getPerimeter());
+        System.out.println(myRectangle.getArea());
+
+
     }
 }
--- src/shapes/Square.java
+++ src/shapes/Square.java
@@ -1,18 +1,43 @@
 package shapes;
 
-class Square extends Rectangle {
+//class Square extends Rectangle {
+//    public Square(int side) {
+//        super(side, side);
+//    }
+//    @Override
+//    public int getArea(){
+//        System.out.println("this is the area");
+//        return length * length;
+//    }
+//
+//    @Override
+//    public int getPerimeter() {
+//        return 4 * length;
+//    }
+//}
 
-    public Square(int side) {
-        super(side, side);
+public class Square extends Quadrilateral {
+    public Square(int length) {
+        super(length, length);
     }
-    @Override
-    public int getArea(){
-        System.out.println("this is the area");
-        return length * length;
+
+    public void setLength(int length) {
+        this.length = length;
+        this.width = length;
+    }
+
+    public void setWidth(int width) {
+        this.width = width;
+        this.length = width;
     }
 
     @Override
-    public int getPerimeter() {
+    public double getPerimeter() {
         return 4 * length;
+    }
+
+    @Override
+    public double getArea() {
+        return length * width;
     }
 }
